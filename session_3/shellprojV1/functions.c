ceb9616fbb3a96a3f0765591b80241a59979aad3
basselmahmoud1 <basselmahmoud1@gmail.com>
2024-07-26 00:55:02 +0300

    adding case handling none supported command

--- session_3/shellprojV1/functions.c
+++ session_3/shellprojV1/functions.c
@@ -406,36 +406,41 @@ void perform_comm (void)
 		pwd ();
 	}
 	
-	if(strcmp(tokens[0],"echo")==0)
+	else if(strcmp(tokens[0],"echo")==0)
 	{
 	//	write_usr("echo is entered\n", strlen("echo is entered\n") );
 		echo ();
 	}
 	
-	if(strcmp(tokens[0],"cp")==0)
+	else if(strcmp(tokens[0],"cp")==0)
 	{
 		cp();
 		//write_usr("cp is entered\n", strlen("cp is entered\n") );
 	}
 	
-	if(strcmp(tokens[0],"mv")==0)
+	else if(strcmp(tokens[0],"mv")==0)
 	{
 		//write_usr("mv is entered\n", strlen("mv is entered\n") );
 		mv();
 	}
 	
 	
-	if(strcmp(tokens[0],"exit")==0)
+	else if(strcmp(tokens[0],"exit")==0)
 	{
 		//write_usr("exit is entered\n", strlen("exit is entered\n") );
 		exit_usr();
 	}
 	
 	
-	if(strcmp(tokens[0],"help")==0)
+	else if(strcmp(tokens[0],"help")==0)
 	{
 		//write_usr("help is entered\n", strlen("help is entered\n") );
 		help();
+	}
+	else
+	{
+		write_usr("command isnt supported\n", strlen("command isnt supported\n") );
+	
 	}
 	
 	
