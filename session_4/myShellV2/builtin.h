893168c445ef08de055eda999367247da43588a2
basselmahmoud1 <basselmahmoud1@gmail.com>
2024-07-30 02:11:53 +0300

    test

---
+++ session_4/myShellV2/builtin.h
@@ -0,0 +1,19 @@
+#ifndef BUILTIN_H
+#define BUILTIN_H
+
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include  <stddef.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+void perform_builtin(int argc,char **argv);
+
+
+
+
+#endif
