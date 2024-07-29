534df2da90a165741aaef7a4a67fe6d88598a0f8
Bassel Mahmoud Ahmed <138879250+basselmahmoud1@users.noreply.github.com>
2024-07-30 01:55:11 +0300

    Update main.c

--- session_4/myShellV2/main.c
+++ session_4/myShellV2/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <errno.h>
+
 #include "functions.h"
+extern char command [1001];
+extern char ** argv;
+extern int argc ;
+int main(void) {
+    while(1) {
+        write_usr(shellmsg,strlen(shellmsg));
+        read_usr();
+        argc = get_tokens_num(command);
+        argv = get_tokens(command);
+        int i=0;
+        for(i=0 ; i<argc ; i++) {
+            printf("%s ",argv[i]);
+        }
+      //      printf("\n");
+        for(i=0 ; i<argc && argv[i] != NULL ; i++)
+            free (argv[i]);
+        free (argv);
 
-
-int main (void)
-{
-	while (1)
-	{
-		
-	}
+    }
 }
