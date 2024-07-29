051fa6f6e9430ed83f8235c00e2c529e13fbaec1
Bassel Mahmoud Ahmed <138879250+basselmahmoud1@users.noreply.github.com>
2024-07-30 01:54:35 +0300

    Update functions.c

--- session_4/myShellV2/functions.c
+++ session_4/myShellV2/functions.c
@@ -4,63 +4,73 @@
 int  counter;
 int error_checker = 0;
 int argc ;
-char * argv[] ;
-char * commands; 
+char ** argv ;
+char  command[COMM_SIZE];
 
 const char *shellmsg ="engez mafesh w2t lel tafser $> ";
 
 
 void read_usr (void)
 {
-	ssize_t readsize = 0;
-	readsize = read (STDIN,command,100);	
-	
-	if (readsize == -1)
-	{
-		perror("read:");
-		error_checker = 1;
-	}
-	if(readsize>1)
-		command [readsize-1]='\0';
-	else 
-		command [readsize+1]='\0';
-	
-		
+    ssize_t readsize = 0;
+    readsize = read (STDIN,command,COMM_SIZE);
+
+    if (readsize == -1)
+    {
+        perror("read:");
+        error_checker = 1;
+    }
+
+        command [readsize-1]='\0';
+
+
+
 }
 
-void get_tokens_num (const char * buff )
+int get_tokens_num (const char * buff )
 {
-	char *tokens ;
-	tokens = strtok( buff , " " );
-	while (tokens[argc] != NULL )
-	{
-		argc++;
-		tokens[argc] = strtok( NULL , " " ); 
-	}
+    char cp [COMM_SIZE] ;
+    strcpy(cp,buff);
+    int counter=0;
+    char *tokens = strtok( cp , " " );
+    while (tokens != NULL )
+    {
+        counter++;
+        tokens = strtok( NULL , " " );
+    }
+    return counter;
 }
 
-void get_tokens (const char * buff )
+char ** get_tokens (const char * buff )
 {
-	char **tokens = malloc(argc * sizeof(char*)) ;
-	tokens = strtok( buff , " " );
-	while (token[argc] != NULL )
-	{
-		argc++;
-		tokens[argc] = strtok( NULL , " " ); 
-	}
-	
+    if (argc == 0)
+        return NULL ;
+    int counter =0;
+    char cp [COMM_SIZE] ;
+    char ** argg;
+    strcpy(cp,buff);
+    argg = malloc(argc * sizeof(char*)) ;
+    char *token  = strtok( cp , " " );
+    while (token != NULL )
+    {
+        argg[counter] = strdup(token);
+        token = strtok( NULL , " " );
+        counter++;
+    }
+    return argg;
+
 }
 
 
 
 void write_usr ( const char *buffer , size_t count)
 {
-	ssize_t writesize = write (STDOUT , buffer , count) ;
-	
-	if( writesize == -1 )
-	{
-		perror("write:");
-		error_checker = 1;
-	}
-	
+    ssize_t writesize = write (STDOUT , buffer , count) ;
+
+    if( writesize == -1 )
+    {
+        perror("write:");
+        error_checker = 1;
+    }
+
 }
